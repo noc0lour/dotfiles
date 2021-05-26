efdfdc55cff9e9831c9a47c7749cc595fc255f66
Andrej Rode <mail@andrejro.de>
2021-05-26 12:10:27 +0200

    Allow using neo2 with spacemacs

--- spacemacs/.spacemacs.d/init.el
+++ spacemacs/.spacemacs.d/init.el
@@ -499,6 +499,8 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
+  (setq mac-option-modifier 'none)
+
   (add-hook 'prog-mode-hook #'(lambda ()
                                 (dtrt-indent-mode)
                                 (dtrt-indent-adapt)))
